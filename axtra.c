b0815e1139b6d9fe4ff67e297134254a41898928
Satya-teja <94442480+Satya-teja@users.noreply.github.com>
2021-12-16 08:12:15 +0000

    star pattern

---
+++ axtra.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int a=5, b=6, c=7;
+
+    printf("%d", a*b/c);
+
+    return 0;
+
+}
---
+++ salary.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+  float da,pf,it,others,ta,hra,net,basic;
+  prntf("Enter input for basic");
+  scanf("%f", &basic);
+  printf("Enter input for others");
+  scanf("%f",&others);
+  printf()
+}
